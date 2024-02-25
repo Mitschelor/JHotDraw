3e730155f2432dd669f49d5c0a9cb3c40594bbea
Mitschelor <michael@schnyder.cc>
2024-02-25 14:05:45 +0100

    Test setup

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/GroupingManagerTest.java
@@ -0,0 +1,20 @@
+package org.jhotdraw.draw.action;
+
+import org.jhotdraw.draw.DrawingView;
+import org.testng.annotations.Test;
+import org.mockito.Mock;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class GroupingManagerTest {
+    @Mock
+    private DrawingView drawingView;
+    @Test
+    public void failTest() {
+    }
+
+    @Test
+    public void passTest() {
+        assertEquals(5, 5);
+    }
+}
