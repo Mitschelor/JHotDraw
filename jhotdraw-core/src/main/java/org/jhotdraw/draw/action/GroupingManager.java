83223c40f20bd7f04121d06f74864919d66b7e53
Michael <michael@scnyder.cc>
2024-02-27 11:23:00 +0100

    Fix sonarlint issues

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/GroupingManager.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/GroupingManager.java
@@ -4,53 +4,55 @@ import org.jhotdraw.draw.DrawingView;
 import org.jhotdraw.draw.figure.CompositeFigure;
 import org.jhotdraw.draw.figure.Figure;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.LinkedList;
 
-public class GroupingManager {
-  private final DrawingView view;
-  private final CompositeFigure group;
-  private final Collection<Figure> figures;
+public class GroupingManager implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final DrawingView view;
+    private final CompositeFigure group;
+    private final Collection<Figure> figures;
 
-  public GroupingManager(DrawingView view, CompositeFigure group,
-                         Collection<Figure> figures) {
-    if (view == null || group == null || figures == null) {
-      throw new IllegalArgumentException("Arguments must not be null.");
+    public GroupingManager(DrawingView view, CompositeFigure group,
+                           Collection<Figure> figures) {
+        if (view == null || group == null || figures == null) {
+            throw new IllegalArgumentException("Arguments must not be null.");
+        }
+        this.view = view;
+        this.group = group;
+        this.figures = figures;
     }
-    this.view = view;
-    this.group = group;
-    this.figures = figures;
-  }
 
 
-  public void groupFigures() {
-    Collection<Figure> sorted = view.getDrawing().sort(figures);
-    if (figures.isEmpty()) {
-      return;
+    public void groupFigures() {
+        Collection<Figure> sorted = view.getDrawing().sort(figures);
+        if (figures.isEmpty()) {
+            return;
+        }
+        int index = view.getDrawing().indexOf(sorted.iterator().next());
+        view.getDrawing().basicRemoveAll(figures);
+        view.clearSelection();
+        view.getDrawing().add(index, group);
+        group.willChange();
+        for (Figure f : sorted) {
+            f.willChange();
+            group.basicAdd(f);
+        }
+        group.changed();
+        view.addToSelection(group);
     }
-    int index = view.getDrawing().indexOf(sorted.iterator().next());
-    view.getDrawing().basicRemoveAll(figures);
-    view.clearSelection();
-    view.getDrawing().add(index, group);
-    group.willChange();
-    for (Figure f : sorted) {
-      f.willChange();
-      group.basicAdd(f);
-    }
-    group.changed();
-    view.addToSelection(group);
-  }
 
-  public Collection<Figure> ungroupFigures() {
-    LinkedList<Figure> figures = new LinkedList<>(group.getChildren());
-    if (figures.isEmpty()) {
-      return figures;
+    public Collection<Figure> ungroupFigures() {
+        LinkedList<Figure> figures = new LinkedList<>(group.getChildren());
+        if (figures.isEmpty()) {
+            return figures;
+        }
+        view.clearSelection();
+        group.basicRemoveAllChildren();
+        view.getDrawing().basicAddAll(view.getDrawing().indexOf(group), figures);
+        view.getDrawing().remove(group);
+        view.addToSelection(figures);
+        return figures;
     }
-    view.clearSelection();
-    group.basicRemoveAllChildren();
-    view.getDrawing().basicAddAll(view.getDrawing().indexOf(group), figures);
-    view.getDrawing().remove(group);
-    view.addToSelection(figures);
-    return figures;
-  }
 }
