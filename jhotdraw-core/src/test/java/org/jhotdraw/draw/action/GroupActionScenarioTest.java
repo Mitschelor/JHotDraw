eda8720d92b37b9aba9d38049473368b0144a55b
Michael <michael@scnyder.cc>
2024-02-27 19:02:30 +0100

    Add ScenarioTest to classpath

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/GroupActionScenarioTest.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/GroupActionScenarioTest.java
@@ -1,4 +1,7 @@
 package org.jhotdraw.draw.action;
 
-public class GroupActionScenarioTest {
+import com.tngtech.jgiven.impl.Scenario;
+import com.tngtech.jgiven.junit.ScenarioTest;
+
+public class GroupActionScenarioTest extends ScenarioTest<GivenGroupAction, WhenGroupAction, ThenGroupAction> {
 }
